b038e4794351cf4dfaae7b21e85bd63116b3a7f2
Frank O'Brien <fobrien@roblox.com>
2023-03-06 19:27:28 -0800

    move lsp on attach to separate file

--- init.lua
+++ init.lua
@@ -19,49 +19,6 @@ vim.g.maplocalleader = " "
 -- Keymap for better default experience
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
--- function to set up lsp-esque behavior when something providing LSP features attaches to a buffer (lspconfig & null-ls)
-local lsp_on_attach = function(client, bufnr)
-    -- lets us more easily define mappings specific
-    -- for LSP related items. It sets the mode, buffer and description for us each time.
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = "LSP: " .. desc
-        end
-
-        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-    end
-
-    nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-    nmap("<leader>ca", ":CodeActionMenu<cr>", "[C]ode [A]ction")
-    -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-    nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-    nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-    nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-    nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-    nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-    nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-    nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-    -- See `:help K` for why this keymap
-    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-    nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-    -- Lesser used LSP functionality
-    nmap("<leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, "[W]orkspace [L]ist Folders")
-
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-        vim.lsp.buf.format()
-    end, { desc = "Format current buffer with LSP" })
-
-    -- Adds command `:FormatModifications`
-    local lsp_format_modifications = require("lsp-format-modifications")
-    lsp_format_modifications.attach(client, bufnr, { format_on_save = false })
-end
-
 -- plugins, TODO: move these to individual files
 require("lazy").setup({
     { -- LSP Configuration & Plugins
@@ -85,7 +42,7 @@ require("lazy").setup({
         config = function()
             local null_ls = require("null-ls")
             null_ls.setup({
-                on_attach = lsp_on_attach,
+                on_attach = require("frank.on-lsp-attach"),
                 sources = {
                     null_ls.builtins.formatting.stylua,
                     null_ls.builtins.formatting.black,
@@ -789,7 +746,7 @@ mason_lspconfig.setup_handlers({
     function(server_name)
         require("lspconfig")[server_name].setup({
             capabilities = capabilities,
-            on_attach = lsp_on_attach,
+            on_attach = require("frank.on-lsp-attach"),
             settings = servers[server_name],
         })
     end,
@@ -800,7 +757,7 @@ require("workspaces").setup({
     hooks = {
         -- open = { "Telescope find_files" },
         open = {
-            function(name, path)
+            function(_, path)
                 require("oil").open(path)
             end,
         },
