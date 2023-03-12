4e3c0f3340fa077f5bd4c6f0d71e3bc01ab8b421
Frank O'Brien <fobrien@roblox.com>
2023-03-11 16:04:35 -0800

    pull out neodev from lspconfig config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -45,8 +45,6 @@ return {
                 },
             }
 
-            require("neodev").setup()
-
             -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
