8f1dafa0703f2dd55411b30c43581fc0b810dceb
Frank O'Brien <fobrien@roblox.com>
2024-01-03 19:20:26 -0800

    cleant fidget config, remove unused toggleterm bindings

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -3,13 +3,9 @@ return {
     config = function()
         local fidget = require("fidget")
         fidget.setup({
-            display = {
-                render_limit = 16,
-            },
             notification = {
                 override_vim_notify = true,
             },
         })
-        vim.notify = fidget.notify
     end,
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -26,24 +26,6 @@ return {
         vim.keymap.set("n", "<leader>tt", ":ToggleTerm<cr>", { silent = true, desc = "[T]oggle [T]erm" })
         vim.keymap.set("n", "<c-t><c-t>", ":ToggleTerm<cr>", { silent = true, desc = "[T]oggle [T]erm" })
         vim.keymap.set("t", "<c-t><c-t>", "<c-\\><c-n>:ToggleTerm<cr>", { silent = true, desc = "[T]oggle [T]erm" })
-        vim.keymap.set(
-            "n",
-            "<leader>tv",
-            ":ToggleTerm direction=vertical <cr>",
-            { silent = true, desc = "Toggle [H]orizontal terminal view" }
-        )
-        vim.keymap.set(
-            "n",
-            "<leader>th",
-            ":ToggleTerm direction=horizontal <cr>",
-            { silent = true, desc = "Toggle [V]ertical terminal view" }
-        )
-        vim.keymap.set(
-            "n",
-            "<leader>ff",
-            ":ToggleTerm direction=float <cr>",
-            { silent = true, desc = "Toggle [F]loat terminal view" }
-        )
 
         -- custom terminal for gitui
         local Terminal = require("toggleterm.terminal").Terminal
