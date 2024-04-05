fd7b6080f9bcbab040ad363bbc2c258435ce64f6
Frank O'Brien <fobrien@roblox.com>
2024-04-05 13:56:03 -0700

    remove unwanted surround keymaps

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -4,15 +4,16 @@ return { -- suround stuff easily
         require("mini.surround").setup({
             mappings = {
                 add = "<leader>wa", -- Add surrounding in Normal and Visual modes
-                replace = "<leader>wr", -- Replace surrounding
-                -- delete = "<leader>wd", -- Delete surrounding
-                -- find = "<leader>wf", -- Find surrounding (to the right)
-                -- find_left = "<leader>wF", -- Find surrounding (to the left)
-                -- highlight = "<leader>wh", -- Highlight surrounding
-                -- update_n_lines = "<leader>wn", -- Update `n_lines`
-                --
-                -- suffix_last = "l", -- Suffix to search with "prev" method
-                -- suffix_next = "n", -- Suffix to search with "next" method
+                delete = "<leader>wd", -- Delete surrounding
+
+                -- disable all the others
+                find = "", -- Find surrounding (to the right)
+                find_left = "", -- Find surrounding (to the left)
+                highlight = "", -- Highlight surrounding
+                replace = "", -- Replace surrounding
+                update_n_lines = "", -- Update `n_lines`
+                suffix_last = "", -- Suffix to search with "prev" method
+                suffix_next = "", -- Suffix to search with "next" method
             },
         })
     end,
