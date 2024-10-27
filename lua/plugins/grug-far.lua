efec39c98eb12bb4038f1f3f77511055dfb3b173
Frank O'Brien <fobrien@roblox.com>
2024-10-27 15:31:09 -0700

    spectre > grug-far

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -2,11 +2,11 @@ return {
     "MagicDuck/grug-far.nvim",
     config = function()
         require("grug-far").setup({})
-        vim.keymap.set(
-            { "n", "v" },
-            "<leader>sp",
-            require("grug-far").open,
-            { desc = "Toggle [S]earch [P]project - global find/replace" }
-        )
+        vim.keymap.set({ "n", "v" }, "<leader>sp", function()
+            require("grug-far").toggle_instance({
+                instanceName = "grug-far main",
+                startInInsertMode = false,
+            })
+        end, { desc = "Toggle [S]earch [P]project - global find/replace" })
     end,
 }
