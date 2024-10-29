b757707ba90e9015ab3bfcdd2dbcddff556a4789
Frank O'Brien <fobrien@roblox.com>
2024-10-29 13:37:34 -0700

    make grug-far buffers transient

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -6,6 +6,7 @@ return {
             require("grug-far").toggle_instance({
                 instanceName = "grug-far main",
                 startInInsertMode = false,
+                transient = true,
             })
         end, { desc = "Toggle [S]earch [P]project - global find/replace" })
     end,
